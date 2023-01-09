87610b1748ea612d99b1ce9099ee0442ffb4be00
up-studio-panorama <122280630+up-studio-panorama@users.noreply.github.com>
2023-01-09 16:18:13 +0100

    Hotspot added

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-area1",
-      "name": "Area1",
+      "id": "0-badevrelse",
+      "name": "Badeværelse",
       "levels": [
         {
           "tileSize": 256,
@@ -24,11 +24,18 @@ var APP_DATA = {
       ],
       "faceSize": 2000,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 1.4845877045166587,
+        "pitch": 0.1865189650101371,
+        "fov": 1.3828466230097007
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.728980796970525,
+          "pitch": 0.0852124188825627,
+          "rotation": 0,
+          "target": "4-entre"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -63,8 +70,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "2-area2",
-      "name": "Area2",
+      "id": "2-kkken-alrum",
+      "name": "Køkken Alrum",
       "levels": [
         {
           "tileSize": 256,
@@ -86,11 +93,24 @@ var APP_DATA = {
       ],
       "faceSize": 2000,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 1.355299647312071,
+        "pitch": 0.128044035209939,
+        "fov": 1.3828466230097007
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 2.614940070937843,
+          "pitch": 0.06471798125342332,
+          "rotation": 0,
+          "target": "4-entre"
+        },
+        {
+          "yaw": -1.571053941817123,
+          "pitch": 0.09373892240373927,
+          "rotation": 0,
+          "target": "6-stue"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -125,8 +145,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "4-area3",
-      "name": "Area3",
+      "id": "4-entre",
+      "name": "Entre",
       "levels": [
         {
           "tileSize": 256,
@@ -152,7 +172,20 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.2456243645953471,
+          "pitch": 0.057919256257198626,
+          "rotation": 0,
+          "target": "0-badevrelse"
+        },
+        {
+          "yaw": 0.5271146871505117,
+          "pitch": 0.057530356465917976,
+          "rotation": 0,
+          "target": "2-kkken-alrum"
+        }
+      ],
       "infoHotspots": []
     },
     {
@@ -187,8 +220,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "6-area4",
-      "name": "Area4",
+      "id": "6-stue",
+      "name": "Stue",
       "levels": [
         {
           "tileSize": 256,
@@ -210,11 +243,18 @@ var APP_DATA = {
       ],
       "faceSize": 2000,
       "initialViewParameters": {
+        "yaw": 0.007738668299387186,
         "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "fov": 1.3828466230097007
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.9381139789994766,
+          "pitch": 0.10181672246287121,
+          "rotation": 0,
+          "target": "2-kkken-alrum"
+        }
+      ],
       "infoHotspots": []
     },
     {
